737e4fe1a7d2790e82d2a891a25280afd219ece3
Quan <quantrung286@gmail.com>
2022-01-15 10:46:11 +0700

    up

--- 12-sidebar-modal/src/context.js
+++ 12-sidebar-modal/src/context.js
@@ -3,13 +3,39 @@ import React, { useState, useContext } from 'react'
 const AppContext = React.createContext()
 
 const AppProvider = ({children}) => {
+    const [ isSidebarOpen, setIsSidebarOpen ] = useState(false)
+    const [ isModalOpen, setIsModalOpen ] = useState(false)
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+
+    const value = {
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        openSidebar,
+        closeSidebar,
+        openModal,
+        closeModal
+    }
+
     return (
-    <AppContext.Provider value='heelo'>
+    <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>)
 }
 
-// custom hook
+// custom hook: must begin with use...
 export const useGlobalContext = () => {
     return useContext(AppContext)
 }
