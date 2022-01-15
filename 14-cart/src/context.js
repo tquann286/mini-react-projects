a062966c3efd24abaa43d6d83142cbbb79bfac40
Quan <quantrung286@gmail.com>
2022-01-15 16:01:27 +0700

    up

--- 14-cart/src/context.js
+++ 14-cart/src/context.js
@@ -1,27 +1,42 @@
 import React, { useState, useContext, useReducer, useEffect } from 'react'
 import cartItems from './data'
 import reducer from './reducer'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
-const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+const initialState = {
+	loading: false,
+	cart: cartItems,
+	total: 0,
+	amount: 0,
+}
 
-  return (
-    <AppContext.Provider
-      value={{
-        cart,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  )
+const AppProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(reducer, initialState)
+
+	const clearCart = () => {
+		dispatch({ type: 'CLEAR_CART' })
+	}
+
+	const remove = (id) => {
+		dispatch({ type: 'REMOVE', payload:id })
+	}
+
+	return (
+		<AppContext.Provider
+			value={{
+				...state,
+				clearCart,
+        remove,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	)
 }
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
+	return useContext(AppContext)
 }
 
 export { AppContext, AppProvider }
