caa11fa824f7dd53f8b82ef3d607e7c069c093a5
Quan <quantrung286@gmail.com>
2022-01-15 10:15:03 +0700

    up

--- 12-sidebar-modal/src/context.js
+++ 12-sidebar-modal/src/context.js
@@ -1,1 +1,17 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) => {
+    return (
+    <AppContext.Provider value='heelo'>
+        {children}
+    </AppContext.Provider>)
+}
+
+// custom hook
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
