fe9a2a52da3bf69c3a0c94ceac26859f5b7e5434
Quan <quantrung286@gmail.com>
2022-01-16 11:48:14 +0700

    up

--- 15-cocktails/src/components/CocktailList.js
+++ 15-cocktails/src/components/CocktailList.js
@@ -4,9 +4,30 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext()
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (cocktails.length < 1) {
+    return (
+      <h2 className='section-title'>
+        no cocktails matched your search criteria
+      </h2>
+    )
+  }
+
   return (
     <div>
-      <h2>cocktail list component</h2>
+      <h2 className='section-title'>
+        cocktails
+      </h2>
+      <div className="cocktails-center">
+        {cocktails.map( item => {
+          return <Cocktail key={item.id} {...item} />
+        })}
+      </div>
     </div>
   )
 }
--- 15-cocktails/src/components/SearchForm.js
+++ 15-cocktails/src/components/SearchForm.js
@@ -2,11 +2,23 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
+	const { setSearchTerm } = useGlobalContext()
+	const searchValue = React.useRef('')
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+
+	return (
+		<section className='section search'>
+			<form className='search-form'>
+				<div className='form-control'>
+					<label htmlFor='name'>search your favorite cocktail</label>
+					<input type='text' id='name' ref={searchValue} onChange={searchCocktail} />
+				</div>
+			</form>
+		</section>
+	)
 }
 
 export default SearchForm
