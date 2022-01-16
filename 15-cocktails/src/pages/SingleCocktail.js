a6b8c43c9a11c5fa36c2c355aa16d353734b6da0
Quan <quantrung286@gmail.com>
2022-01-16 12:02:40 +0700

    up

--- 15-cocktails/src/pages/SingleCocktail.js
+++ 15-cocktails/src/pages/SingleCocktail.js
@@ -4,9 +4,14 @@ import { useParams, Link } from 'react-router-dom'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
+  const {id} = useParams()
+  const [loading, setLoading] = React.useState(false)
+  const [cocktail, setCocktail] = React.useState(null)
+
+  console.log(useParams())
   return (
     <div>
-      <h2>single cocktail page </h2>
+      <h2>{id}</h2>
     </div>
   )
 }
